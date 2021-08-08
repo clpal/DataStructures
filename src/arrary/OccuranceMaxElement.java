e6211a11efa8886d7193c54e9c5f9f219be0701d
CL Pal <clpalmca@gmail.com>
2021-08-08 10:24:02 +0530

    title
    clpalpal ka comment check

--- src/arrary/OccuranceMaxElement.java
+++ src/arrary/OccuranceMaxElement.java
@@ -1,7 +1,7 @@
 package arrary;
 
 public class OccuranceMaxElement {
-  static   int[] arr = {1,1,2,1,5,6,6,6,8,5,9,7,1};
+  static   int[] arr = {1,1,2,1,5,6,6,6,8,5,9,7,7,1};
     public static void main(String[] args) {
          int count=0;
         for (int i = 0; i <arr.length ; i++) {
