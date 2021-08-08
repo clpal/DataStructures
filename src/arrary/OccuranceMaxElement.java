c8d393ac1c2b52e301acd52c5dbc6b8d77d3987c
CL Pal <clpalmca@gmail.com>
2021-08-08 07:04:31 +0530

    occurance of element

--- src/arrary/OccuranceMaxElement.java
+++ src/arrary/OccuranceMaxElement.java
@@ -1,8 +1,16 @@
 package arrary;
 
 public class OccuranceMaxElement {
-    int[] arr = {1,1,2,1,5,6,6,6,8,5,9,7,1};
+  static   int[] arr = {1,1,2,1,5,6,6,6,8,5,9,7,1};
     public static void main(String[] args) {
-
+         int count=0;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]==arr[j+1]){
+                    count++;
+                }
+            }
+        }
+System.out.println(count);
     }
 }
