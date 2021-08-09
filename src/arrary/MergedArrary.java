92b7c9e96f4ea86fdc7bf2b3953627617c25da6e
CL Pal <clpalmca@gmail.com>
2021-08-09 06:42:11 +0530

    testdemo
    merged test

--- src/arrary/MergedArrary.java
+++ src/arrary/MergedArrary.java
@@ -1,7 +1,7 @@
 package arrary;
 
 public class MergedArrary {
-
+ String testdemo="merged test";
     public static void main(String[] args) {
         int nums1[]={1,2,3,0,0,0};
         int nums2[]={2,5,6};
